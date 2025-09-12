8e027803bed2340cabd12669adad059bc31b6466
shruti198181 <shahshruti503@gmail.com>
2025-09-12 11:51:31 +0530

    changes

--- src/manager/project.jsx
+++ src/manager/project.jsx
@@ -1,53 +1,129 @@
-"use client"
+// "use client"
 
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../component/table"
+// import {
+//   Table,
+//   TableBody,
+//   TableCaption,
+//   TableCell,
+//   TableHead,
+//   TableHeader,
+//   TableRow,
+// } from "../component/table"
 
-export default function Projects() {
-  return (
-    <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
-      <div className=" p-6 rounded-2xl">
-        <h2 className="text-2xl font-semibold mb-4">Projects</h2>
+// export default function Projects() {
+//   return (
+//     <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+//       <div className=" p-6 rounded-2xl">
+//         <h2 className="text-2xl font-semibold mb-4">Projects</h2>
 
-        <Table>
+//         <Table>
          
+//           <TableHeader>
+//             <TableRow>
+//               <TableHead className="w-[100px] border border-gray-800">Project ID</TableHead>
+//               <TableHead className="border border-gray-800">Name</TableHead>
+//               <TableHead className="border border-gray-800">Status</TableHead>
+//               <TableHead className="border border-gray-800">Deadline</TableHead>
+//             </TableRow>
+//           </TableHeader>
+//           <TableBody>
+//             <TableRow>
+//               <TableCell className="font-medium border border-gray-800">101</TableCell>
+//               <TableCell className="border border-gray-800">Website Redesign</TableCell>
+//               <TableCell className="border border-gray-800">In Progress</TableCell>
+//               <TableCell className="border border-gray-800">2025-09-15</TableCell>
+//             </TableRow>
+//             <TableRow>
+//               <TableCell className="font-medium border border-gray-800">102</TableCell>
+//               <TableCell className="border border-gray-800">Mobile App</TableCell>
+//               <TableCell className="border border-gray-800">Completed</TableCell>
+//               <TableCell className="border border-gray-800">2025-08-28</TableCell>
+//             </TableRow>
+//             <TableRow>
+//               <TableCell className="font-medium border border-gray-800">103</TableCell>
+//               <TableCell className="border border-gray-800">Admin Dashboard</TableCell>
+//               <TableCell className="border border-gray-800">Pending</TableCell>
+//               <TableCell className="border border-gray-800">2025-10-10</TableCell>
+//             </TableRow>
+//           </TableBody>
+//         </Table>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+"use client"
+import React, {useState,useEffect} from  'react';
+import { TableHeader, TableRow ,TableHead,Table, TableCell,TableBody} from '../component/table';
+
+export default function Projects () {
+   const [data,setData] = useState([]);
+   const [loading,setLoading] = useState(true);
+   const [page,setpage] = useState(1);
+   const limit = 10;
+   const totalPages = Math.ceil(data.length / limit);
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const currentTodos = data.slice((page - 1) * limit, page * limit);
+
+   useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+    .then((response) => response.json())
+    .then((json)=>setData(json))
+    .catch((err) =>console.error("fetch error",err))
+    .finally(() =>setLoading(false)) 
+   },[])
+
+   if(loading) return<p className='p-4 text-center'>Loading</p>
+
+   return(
+    <div className='p-4'>
+      <h2 className='text-xl font-bold mb-4 text-center'>Projects</h2>
+        <Table className="border border-gray-300 text-center border-collapse min-w-[700px] mx-auto">
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px] border border-gray-800">Project ID</TableHead>
-              <TableHead className="border border-gray-800">Name</TableHead>
-              <TableHead className="border border-gray-800">Status</TableHead>
-              <TableHead className="border border-gray-800">Deadline</TableHead>
+              <TableHead className='border border-gray-300'>Id</TableHead>
+              <TableHead className='border border-gray-300'>User Id</TableHead>
+              <TableHead className='border border-gray-300'>Title</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-medium border border-gray-800">101</TableCell>
-              <TableCell className="border border-gray-800">Website Redesign</TableCell>
-              <TableCell className="border border-gray-800">In Progress</TableCell>
-              <TableCell className="border border-gray-800">2025-09-15</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium border border-gray-800">102</TableCell>
-              <TableCell className="border border-gray-800">Mobile App</TableCell>
-              <TableCell className="border border-gray-800">Completed</TableCell>
-              <TableCell className="border border-gray-800">2025-08-28</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="font-medium border border-gray-800">103</TableCell>
-              <TableCell className="border border-gray-800">Admin Dashboard</TableCell>
-              <TableCell className="border border-gray-800">Pending</TableCell>
-              <TableCell className="border border-gray-800">2025-10-10</TableCell>
-            </TableRow>
+            {currentTodos.map((menu) => (
+              <TableRow key={menu.id}>
+                <TableCell className='border border-gray-300'>{menu.id}</TableCell>
+                <TableCell className='border border-gray-300'>{menu.userId}</TableCell>
+                <TableCell className='border border-gray-300'>{menu.title}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
+         <div className="flex justify-center items-center mt-4 gap-2   text-center">
+        <button
+          className={`btn btn-sm btn-primary ${page === 1 ? "opacity-50 cursor-not-allowed" : ""}`}
+          disabled={page === 1}
+          onClick={() => setpage((prev) => Math.max(prev - 1, 1))}
+        >
+          Previous
+        </button>
+
+        {pages.map((p) => (
+          <button
+            key={p}
+            className={`btn btn-sm ${p === page ? "btn-secondary" : "btn-outline"}`}
+            onClick={() => setpage(p)}
+          >
+            {p}
+          </button>
+        ))}
+
+        <button
+          className={`btn btn-sm btn-primary ${page === totalPages ? "opacity-50 cursor-not-allowed" : ""}`}
+          disabled={page === totalPages}
+          onClick={() => setpage((prev) => Math.min(prev + 1, totalPages))}
+        >
+          Next
+        </button>
       </div>
     </div>
-  )
+   )
 }
