70bab654df98c2f65949f9ca518f1b80c54b4852
shruti198181 <shahshruti503@gmail.com>
2025-09-16 11:04:10 +0530

    changes

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -21,55 +21,89 @@ export default function NavbarHeader() {
   };
 
   return (
-    <> 
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            My Dashboard
-          </Navbar.Brand>
+    <>
+      {/* 🔹 Apply background ONLY if user not logged in */}
+      <div
+        style={
+          !role
+            ? {
+                minHeight: "100vh",
+                backgroundImage:
+                  "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNFpZUusVnBdUffCn-Z5tFD9Htx_fYwkA2Eg&s')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }
+            : {}
+        }
+      >
+        {/* Navbar */}
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              My Dashboard
+            </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
-              <NavDropdown title="Admin" id="admin-dropdown">
-                <NavDropdown.Item as={Link} to="/admin/users">Users</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/admin/setting">Settings</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/admin/reports">Reports</NavDropdown.Item>
-              </NavDropdown>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto">
+                <NavDropdown title="Admin" id="admin-dropdown">
+                  <NavDropdown.Item as={Link} to="/admin/users">
+                    Users
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/admin/setting">
+                    Settings
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/admin/reports">
+                    Reports
+                  </NavDropdown.Item>
+                </NavDropdown>
 
-              <NavDropdown title="Manager" id="manager-dropdown">
-                <NavDropdown.Item as={Link} to="/manager/project">Projects</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/manager/team">Team</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/manager/reports">Reports</NavDropdown.Item>
-              </NavDropdown>
+                <NavDropdown title="Manager" id="manager-dropdown">
+                  <NavDropdown.Item as={Link} to="/manager/project">
+                    Projects
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/manager/team">
+                    Team
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/manager/reports">
+                    Reports
+                  </NavDropdown.Item>
+                </NavDropdown>
 
-              <NavDropdown title="User" id="user-dropdown">
-                <NavDropdown.Item as={Link} to="/user/myprofile">My Profile</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/user/task">Tasks</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+                <NavDropdown title="User" id="user-dropdown">
+                  <NavDropdown.Item as={Link} to="/user/myprofile">
+                    My Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/user/task">
+                    Tasks
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
 
-            <Nav>
-              {!role ? (
-                <Button variant="outline-light" onClick={handleLoginOpen}>
-                  Login
-                </Button>
-              ) : (
-                <Button
-                  variant="outline-danger"
-                  onClick={() => {
-                    setRole(null);
-                    navigate("/");
-                  }}
-                >
-                  Logout
-                </Button>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+              <Nav>
+                {!role ? (
+                  <Button variant="outline-light" onClick={handleLoginOpen}>
+                    Login
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outline-danger"
+                    onClick={() => {
+                      setRole(null);
+                      navigate("/");
+                    }}
+                  >
+                    Logout
+                  </Button>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
 
+      {/* Login Modal */}
       <Modal show={showLogin} onHide={handleLoginClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>Login</Modal.Title>
